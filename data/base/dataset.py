e328ad7b7bfad62fa59b3f082b887b276ea67911
BlueberryOreo <1761988645@qq.com>
2023-05-01 22:41:55 +0800

    update dataset

--- data/base/dataset.py
+++ data/base/dataset.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
-
+from data.task_type import TaskType
 from torch.utils.data import Dataset
 
 
 class AnomalousDataset(Dataset, ABC):
-    def __init__(self, task):
+    def __init__(self, task: TaskType):
         super().__init__()
+        self.task = task
