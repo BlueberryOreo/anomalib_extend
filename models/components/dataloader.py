045c4ed82ec038c7dd0dea62a83e4ef7cb3dea98
BlueberryOreo <1761988645@qq.com>
2023-05-01 21:50:22 +0800

    update components

--- models/components/dataloader.py
+++ models/components/dataloader.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from torch.utils.data import DataLoader
 
 
 class DataModule(ABC):
@@ -8,4 +9,6 @@ class DataModule(ABC):
             eval_batch_size: int,
 
     ):
+        self.train_batch_size = train_batch_size
+        self.eval_batch_size = eval_batch_size
         pass
