d03cf5eb3ea0c8dbabccee708221fb229b06dfea
BlueberryOreo <1761988645@qq.com>
2023-05-06 21:22:01 +0800

    update dataset.py

--- data/base/dataset.py
+++ data/base/dataset.py
@@ -13,7 +13,7 @@ class AnomalousDataset(Dataset, ABC):
         super().__init__()
         self.task = task
         self.transform = transform
-        self._samples: DataFrame
+        self._samples: DataFrame = None
 
     def is_setup(self) -> bool:
         return isinstance(self._samples, DataFrame)
@@ -23,10 +23,21 @@ class AnomalousDataset(Dataset, ABC):
             raise RuntimeError("Dataset is not set up yet. Call setup() first.")
         return self._samples
 
+    def set_samples(self, samples: DataFrame):
+        assert isinstance(samples, DataFrame), "samples must be a pandas.DataFrame, found {}".format(type(samples))
+        self._samples = samples.sort_values(by="image_path", ignore_index=True)
+
     def setup(self) -> None:
         if not self.is_setup():
             self._setup()
         assert self.is_setup(), "setup() should set self._samples"
+
+    def __getitem__(self, index: int):
+
+        image_path = self._samples.iloc[index].image_path
+        mask_path = self._samples.iloc[index].mask_path
+        label_index = self._samples.iloc[index].label_index
+
 
     @abstractmethod
     def _setup(self) -> DataFrame:
