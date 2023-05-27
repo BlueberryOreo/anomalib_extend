35e8633515b1d38f38f7e0b01cb61d96a79ca092
BlueberryOreo <1761988645@qq.com>
2023-05-27 22:26:49 +0800

    update dataloader.py, dataset.py

--- src/anomalib_extend/data/base/dataloader.py
+++ src/anomalib_extend/data/base/dataloader.py
@@ -6,25 +6,22 @@
 from __future__ import annotations
 
 import logging
-
-from typing import Dict, List, Any
-
 from abc import ABC
+from typing import Any, Dict, List, Optional
+
+from pandas import DataFrame
+from pytorch_lightning import LightningDataModule
 from pytorch_lightning.utilities.types import EVAL_DATALOADERS, TRAIN_DATALOADERS
 from torch.utils.data import DataLoader, default_collate
-from typing import Optional
 
-from pytorch_lightning import LightningDataModule
-from pandas import DataFrame
-
+from anomalib_extend.data.base.dataset import AnomalibDataset
+from anomalib_extend.data.synthetic import SyntheticAnomalyDataset
 from anomalib_extend.data.utils import (
     TestSplitMode,
     ValSplitMode,
     random_split,
-    split_by_label
+    split_by_label,
 )
-from anomalib_extend.data.synthetic import SyntheticAnomalyDataset
-from anomalib_extend.data.base.dataset import AnomalibDataset
 
 logger = logging.getLogger(__name__)
 
@@ -53,25 +50,40 @@ def collate_fn(batch: List) -> Dict[str, Any]:
 
 
 class AnomalibDataModule(LightningDataModule, ABC):
+    """Base Anomalib data module.
+
+    Args:
+        train_batch_size (int): Batch size used by the train dataloader.
+        test_batch_size (int): Batch size used by the val and test dataloaders.
+        num_workers (int): Number of workers used by the train, val and test dataloaders.
+        test_split_mode (Optional[TestSplitMode], optional): Determines how the test split is obtained.
+            Options: [none, from_dir, synthetic]
+        test_split_ratio (float): Fraction of the train images held out for testing.
+        val_split_mode (ValSplitMode): Determines how the validation split is obtained. Options: [none, same_as_test,
+            from_test, synthetic]
+        val_split_ratio (float): Fraction of the train or test images held our for validation.
+        seed (Optional[int], optional): Seed used during random subset splitting.
+    """
+
     def __init__(
-            self,
-            train_batch_size: int,
-            eval_batch_size: int,
-            num_workers: int,
-            val_split_mode: ValSplitMode,
-            val_split_ratio: float,
-            test_split_mode: Optional[TestSplitMode] = None,
-            test_split_ratio: Optional[float] = None,
-            seed: Optional[int] = None
+        self,
+        train_batch_size: int,
+        eval_batch_size: int,
+        num_workers: int,
+        val_split_mode: ValSplitMode,
+        val_split_ratio: float,
+        test_split_mode: Optional[TestSplitMode] = None,
+        test_split_ratio: Optional[float] = None,
+        seed: Optional[int] = None,
     ):
         super().__init__()
         self.train_batch_size = train_batch_size
         self.eval_batch_size = eval_batch_size
         self.num_workers = num_workers
-        self.val_split_mode = val_split_mode
-        self.val_split_ratio = val_split_ratio
         self.test_split_mode = test_split_mode
         self.test_split_ratio = test_split_ratio
+        self.val_split_mode = val_split_mode
+        self.val_split_ratio = val_split_ratio
         self.seed = seed
 
         self.train_data: Optional[AnomalibDataset] = None
@@ -79,8 +91,8 @@ class AnomalibDataModule(LightningDataModule, ABC):
         self.test_data: Optional[AnomalibDataset] = None
 
         self._samples: Optional[DataFrame] = None
-    
-    def setup(self, stage: Optional[str] = None) -> None:
+
+    def setup(self, stage: Optional[str] = None):
         """Setup train, validation and test data.
 
         Args:
@@ -88,7 +100,8 @@ class AnomalibDataModule(LightningDataModule, ABC):
         """
         if not self.is_setup:
             self._setup(stage)
-    
+        assert self.is_setup
+
     def _setup(self, _stage: Optional[str] = None) -> None:
         """Set up the datasets and perform dynamic subset splitting.
 
@@ -105,6 +118,9 @@ class AnomalibDataModule(LightningDataModule, ABC):
         self.train_data.setup()
         self.test_data.setup()
 
+        self._create_test_split()
+        self._create_val_split()
+
     def _create_test_split(self):
         """Obtain the test set based on the settings in the config."""
         if self.test_data.has_normal:
@@ -114,18 +130,18 @@ class AnomalibDataModule(LightningDataModule, ABC):
             # when the user did not provide any normal images for testing, we sample some from the training set,
             # except when the user explicitly requested no test splitting.
             logger.info(
-                "No normal test image found. Sampling from training set using a split ratio of %d",
-                self.test_split_ratio
+                "No normal test images found. Sampling from training set using a split ratio of %d",
+                self.test_split_ratio,
             )
             self.train_data, normal_test_data = random_split(self.train_data, self.test_split_ratio)
-        
+
         if self.test_split_mode == TestSplitMode.FROM_DIR:
             self.test_data += normal_test_data
         elif self.test_split_mode == TestSplitMode.SYNTHETIC:
             self.test_data = SyntheticAnomalyDataset.from_dataset(normal_test_data)
         elif self.test_split_mode != TestSplitMode.NONE:
-            raise ValueError("Unsupported Test Split Mode: {}".format(self.test_split_mode))
-    
+            raise ValueError(f"Unsupported Test Split Mode: {self.test_split_mode}")
+
     def _create_val_split(self):
         """Obtain the validation set based on the settings in the config."""
         if self.val_split_mode == ValSplitMode.FROM_TEST:
@@ -142,25 +158,25 @@ class AnomalibDataModule(LightningDataModule, ABC):
             self.val_data = SyntheticAnomalyDataset.from_dataset(normal_val_data)
         elif self.val_split_mode != ValSplitMode.NONE:
             raise ValueError(f"Unknown validation split mode: {self.val_split_mode}")
-    
+
     @property
-    def is_setup(self) -> bool:
-        """Check if setup() has been called."""
+    def is_setup(self):
+        """Checks if setup() has been called."""
         # at least one of [train_data, val_data, test_data] should be setup
         if self.train_data is not None and self.train_data.is_setup:
             return True
-        elif self.val_data is not None and self.val_data.is_setup:
+        if self.val_data is not None and self.val_data.is_setup:
             return True
-        elif self.test_data is not None and self.test_data.is_setup:
+        if self.test_data is not None and self.test_data.is_setup:
             return True
         return False
-    
+
     def train_dataloader(self) -> TRAIN_DATALOADERS:
         """Get train dataloader."""
         return DataLoader(
             dataset=self.train_data, shuffle=True, batch_size=self.train_batch_size, num_workers=self.num_workers
         )
-    
+
     def val_dataloader(self) -> EVAL_DATALOADERS:
         """Get validation dataloader."""
         return DataLoader(
@@ -168,9 +184,9 @@ class AnomalibDataModule(LightningDataModule, ABC):
             shuffle=False,
             batch_size=self.eval_batch_size,
             num_workers=self.num_workers,
-            collate_fn=collate_fn
+            collate_fn=collate_fn,
         )
-    
+
     def test_dataloader(self) -> EVAL_DATALOADERS:
         """Get test dataloader."""
         return DataLoader(
@@ -178,5 +194,5 @@ class AnomalibDataModule(LightningDataModule, ABC):
             shuffle=False,
             batch_size=self.eval_batch_size,
             num_workers=self.num_workers,
-            collate_fn=collate_fn
+            collate_fn=collate_fn,
         )
